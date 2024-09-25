467494c806d7baa9ef04ffae515f02086439fbdf
Avanthika Y <yavanthika@ceiamerica.com>
2024-09-25 11:33:00 +0530

    Update bbs-migration.sh

--- bbs-migration.sh
+++ bbs-migration.sh
@@ -18,23 +18,23 @@ SPECIFIC_PROJECT_KEY=$1
 SPECIFIC_REPO_NAME=$2
 
 # Function to migrate individual repository
-migrate_repo() {
-  local project_key=$1
-  local repo_slug=$2
+# migrate_repo() {
+  # local project_key=$1
+  # local repo_slug=$2
 
-  echo "Migrating repository: $repo_slug from project: $project_key"
+  # echo "Migrating repository: $repo_slug from project: $project_key"
 
   # Run GEI migration command
   gh bbs2gh migrate-repo --bbs-server-url "$BITBUCKET_SERVER_URL" \
-  --bbs-project "$project_key" --bbs-repo "hello-world-app" \
-  --github-org "$GITHUB_ORG" --github-repo "$repo_slug"
+  --bbs-project "$SPECIFIC_PROJECT_KEY" --bbs-repo "$SPECIFIC_REPO_NAME" \
+  --github-org "$GITHUB_ORG" --github-repo "$SPECIFIC_REPO_NAME"
 
   echo "Repository $repo_slug from project $project_key migrated successfully!"
-}
+# }
 
 # If a specific project and repo name is provided, migrate only that repo
-if [ -n "$SPECIFIC_PROJECT_KEY" ] && [ -n "$SPECIFIC_REPO_NAME" ]; then
-  migrate_repo "$SPECIFIC_PROJECT_KEY" "$SPECIFIC_REPO_NAME"
-fi
+# if [ -n "$SPECIFIC_PROJECT_KEY" ] && [ -n "$SPECIFIC_REPO_NAME" ]; then
+#   migrate_repo "$SPECIFIC_PROJECT_KEY" "$SPECIFIC_REPO_NAME"
+# fi
 
 echo "Migration process completed."
